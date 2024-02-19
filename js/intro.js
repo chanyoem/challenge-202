dd3b05bd6d649c868fc984bc9f106a0f69f94913
chanyoem <chanyoem@uci.edu>
2024-02-18 21:55:06 -0800

    animated box when clicked

--- js/intro.js
+++ js/intro.js
@@ -1,2 +1,17 @@
 //resource link: https://www.w3schools.com/jquery/default.asp
 
+$(document).ready(function() {
+    $("#toggle-box").hide();
+
+    $("#toggle-button").click(function() {
+        $("#toggle-box").toggle();
+    });
+
+    $("#toggle-box").click(function() {
+        $(".box").addClass("animate_box");
+
+        setTimeout(function(){
+            $(".box").removeClass("animate_box");
+        }, 10000);
+    });
+});
